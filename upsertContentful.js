c70ae1e859f07b573682bc325ae8113052325b33
Leon Thorne <ldthorne96@gmail.com>
2017-07-14 17:10:46 -0400

    Try and fail at implementing async/await

--- upsertContentful.js
+++ upsertContentful.js
@@ -6,14 +6,14 @@ const client = contentful.createClient({
   accessToken: process.env.ACCESS_TOKEN
 });
 
-const updateEntries = (updatedEntries) => {
+const updateEntries = async (updatedEntries) => {
   return client.getSpace(process.env.PRODUCTION_SPACE_ID)
   .then( (space) => {
     return updatedEntries.map( (entry) => {
       return space.getEntry(entry.sys.id)
-      .then( (foundEntry) => {
-        // merge found entry and exist entry fields
-      })
+      // .then( (foundEntry) => {
+      //   // merge found entry and exist entry fields
+      // })
     })
   })
 }
@@ -22,27 +22,29 @@ const addEntries = (newEntries) => {
 
 }
 
-const upsertEntries = (updatedEntries, newEntries) => {
+const upsertEntries = async (updatedEntries, newEntries) => {
   if (updatedEntries.length) {
-    updateEntries(updatedEntries);
+    await updateEntries(updatedEntries);
   }
-  if (newEntries.length) {
-    addEntries(newEntries);
-  }
+  // if (newEntries.length) {
+  //   addEntries(newEntries);
+  // }
 } 
 
-const upsertContentful = () => {
+const upsertContentful = async () => {
   return dumpAndDiff()
   .then( (setOfDifferences) => {
-    setOfDifferences.forEach( ({type, updatedContent, newContent}) => {
-      if (type === 'Entries') {
-        upsertEntries(updatedContent, newContent);
-      }
-    })
+    const entries = setOfDifferences.find(set => set.type === 'Entries');
+    await upsertEntries(entries.updatedContent, entries.newContent);
+    // setOfDifferences.forEach( ({type, updatedContent, newContent}) => {
+    //   if (type === 'Entries') {
+    //     await upsertEntries(updatedContent, newContent);
+    //   }
+    // })
   })
   .catch( (err) => {
     console.error('Error');
     throw new Error(err);
   });
 }
-upsertContentful();
+await upsertContentful();
